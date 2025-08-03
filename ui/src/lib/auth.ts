7a0918cb90c4bbda74d09807249788cf4e684c5d
shonubijerry <korede@verifi.ng>
2025-08-03 03:01:35 +0100

    fix authentication with zoho

--- ui/src/lib/auth.ts
+++ ui/src/lib/auth.ts
@@ -1,5 +1,4 @@
-// lib/auth.ts
-import type { NextAuthOptions } from 'next-auth'
+import type { NextAuthOptions, Profile, TokenSet } from 'next-auth'
 import ZohoProvider from 'next-auth/providers/zoho'
 
 export const authOptions: NextAuthOptions = {
@@ -7,9 +6,51 @@ export const authOptions: NextAuthOptions = {
     strategy: 'jwt',
   },
   providers: [
+    /**
+     * @todo - Remove token and userinfo once cloudflare support request.https on the server side.
+     * pull-request https://github.com/cloudflare/workers-sdk/pull/10150
+     */
     ZohoProvider({
       clientId: process.env.ZOHO_CLIENT_ID!,
       clientSecret: process.env.ZOHO_CLIENT_SECRET!,
+      token: {
+        url: 'https://accounts.zoho.com/oauth/v2/token',
+        request: async (context) => {
+          const res = await fetch('https://accounts.zoho.com/oauth/v2/token', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+              client_id: context.provider.clientId!,
+              client_secret: context.provider.clientSecret!,
+              grant_type: 'authorization_code',
+              code: context.params.code!,
+              redirect_uri: context.provider.callbackUrl!,
+            }),
+          })
+
+          return { tokens: (await res.json()) as TokenSet }
+        },
+      },
+      userinfo: {
+        url: 'https://accounts.zoho.com/oauth/user/info',
+        request: async ({tokens}) => {
+          const res = await fetch('https://accounts.zoho.com/oauth/user/info', {
+            headers: {
+              Authorization: `Bearer ${tokens.access_token}`,
+            },
+          })
+
+          if (!res.ok) {
+            throw new Error(
+              `Failed to fetch user info from Zoho: ${res.statusText}`,
+            )
+          }
+
+          return res.json() as Profile
+        },
+      },
     }),
   ],
   callbacks: {
